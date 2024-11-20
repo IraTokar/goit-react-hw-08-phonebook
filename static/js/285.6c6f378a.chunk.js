2cc8c76d3214b0ca370d247f0c5ff59d727d2bd9
IraTokar <142804702+IraTokar@users.noreply.github.com>
2024-11-20 12:48:45 +0000

    Deploying to gh-pages from @ IraTokar/goit-react-hw-08-phonebook@68b27a44dc75de36809617aa9d35f581c9b948dc 🚀

--- static/js/285.6c6f378a.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[285],{285:function(e,t,r){r.r(t),r.d(t,{default:function(){return c}});var n=r(184);function c(){return(0,n.jsx)(n.Fragment,{children:(0,n.jsx)("h1",{children:"Register"})})}}}]);
-//# sourceMappingURL=285.6c6f378a.chunk.js.map
--- static/js/910.8ac8fc01.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[910],{910:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var r=n(184);function c(){return(0,r.jsx)("h1",{children:"Contacts"})}}}]);
-//# sourceMappingURL=910.8ac8fc01.chunk.js.map
