591d61588ec7fe5a8bc5ed7c14d1dcd5d5519c01
IraTokar <tokar.ira20@gmail.com>
2024-11-20 14:54:23 +0100

    add auth

--- src/components/redux/auth/authSelectors.js
+++ src/components/redux/auth/authSelectors.js
@@ -1,5 +1,5 @@
-export const selectIsLoggedIn = state = state.auth.isLoggedIn;
+export const selectIsLoggedIn = state => state.auth.isLoggedIn;
 
-export const selectUserName = state = state.auth.user.name;
+export const selectUserName = state => state.auth.user.name;
 
-export const selectIsRefreshing = state.auth.isRefreshing;
+export const selectIsRefreshing = state=> state.auth.isRefreshing;
